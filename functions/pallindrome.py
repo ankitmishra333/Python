add519e17736e6c4cf63a4a29669fb6abdf923c5
unknown <ankitkumarmishra952@gmail.com>
2025-07-16 23:15:05 +0530

    p

---
+++ functions/pallindrome.py
@@ -0,0 +1,10 @@
+def pallindrome(st):
+    if st==st[::-1]:
+        print("pallindrome")
+    else:
+        print("not a pallindrome")
+     
+pallindrome("madam")
+pallindrome("hello")
+pallindrome("12321")
+    
